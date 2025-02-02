51fabdbaeed6a1928057eaecb10642bd6a7da706
jerry12543 <jerryzhu@umich.edu>
2025-02-01 21:11:32 -0500

    routes change

--- LoanMinnow/api/routes.py
+++ LoanMinnow/api/routes.py
@@ -1,7 +1,7 @@
 import flask
 from flask import Blueprint, request, jsonify
 import flask_login
-from flask_login import login_required
+from flask_login import login_required, current_user
 import api.score
 from model import db, User, Pledge, Venture
 from sqlalchemy import func
@@ -32,23 +32,19 @@ def dashboard():
 
     n = 3 # number of top supported and top created ventures to show
 
-    user_id = flask_login.current_user.id
-    score = api.score.get_score(user_id)
+    user = current_user
+    score = api.score.get_score(user)
     
-    available_credits = db.session.query(
-        User.available_credits.filter(User.id == user_id)
-    ).scalar()
+    available_credits = user.available_credits
 
-    credits_invested = db.session.query(
-        func.coalesce(func.sum(Pledge.amount), 0.0).filter(Pledge.lender_id == user_id)
-    ).scalar()
+    credits_invested = sum(pledge.amount for pledge in user.pledges)
 
     top_supported = db.session.query(
         func.sum(Pledge.amount).label("total_amount_invested"), Venture
         ).join(
             Venture, Pledge.venture_id == Venture.id
         ).filter(
-            Pledge.lender_id == user_id
+            Pledge.lender_id == user.id
         ).group_by(
             Venture.id
         ).order_by(
@@ -60,19 +56,37 @@ def dashboard():
         ).join(
             Venture, Pledge.venture_id == Venture.id
         ).filter(
-            Pledge.recipient_id == user_id
+            Pledge.recipient_id == user.id
         ).group_by(
             Venture.id
         ).order_by(
             func.sum(Pledge.amount).desc()
         ).limit(n).all()
 
+    top_supported_data = [
+        {
+            "venture_id": venture.id,
+            "venture_name": venture.name,
+            "total_amount_invested": total_amount_invested
+        }
+        for total_amount_invested, venture in top_supported
+    ]
+    top_created_data = [
+        {
+            "venture_id": venture.id,
+            "venture_name": venture.name,
+            "total_amount_invested": total_amount_invested
+        }
+        for total_amount_invested, venture in top_created
+    ]
+
+
     context = {
         "score": score,
         "available_credits": available_credits,
         "credits_invested": credits_invested,
-        "top_supported": top_supported,
-        "top_created": top_created
+        "top_supported": top_supported_data,
+        "top_created": top_created_data
     }
     return jsonify(**context)
 
